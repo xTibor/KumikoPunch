00b4387d7603ee365e5089e989776a3edecfba87
Nagy Tibor <xnagytibor@protonmail.com>
2025-07-10 22:07:37 +0200

    Add license, reproducible build

--- build.sh
+++ build.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/env bash
 
+# https://github.com/googlefonts/fontmake/issues/379
+# https://reproducible-builds.org/docs/source-date-epoch/#git
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 fontforge -script fontforge-convert.pe src/KumikoPunch-Thin.sfd
 fontforge -script fontforge-convert.pe src/KumikoPunch-Regular.sfd
 fontforge -script fontforge-convert.pe src/KumikoPunch-Bold.sfd
 
-fontmake                                           \
-    --output         variable                      \
-    --output-path    otf/KumikoPunch-VF.otf      \
+fontmake                                         \
+    --output         variable                    \
+    --output-path    build/KumikoPunch.otf       \
     --mm-designspace src/KumikoPunch.designspace \
     --keep-overlaps
 
 rm -rf src/KumikoPunch-Thin.ufo
 rm -rf src/KumikoPunch-Regular.ufo
 rm -rf src/KumikoPunch-Bold.ufo
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+rm -f build/KumikoPunch.zip
+
+zip                       \
+    -X --junk-paths       \
+    build/KumikoPunch.zip \
+    build/KumikoPunch.otf \
+    OFL.txt               \
+    README.md
+
+ziptime build/KumikoPunch.zip
